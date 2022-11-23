7c127e155edfd70c97929d0462e6695b89006e6f
liath-cloud <115594751+liath-cloud@users.noreply.github.com>
2022-11-23 18:22:13 +0000

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,15 @@
 def my_split(text):
-    return [text]
+
+    answer=[]
+    string = ""
+
+    for i in range(len(text)):
+
+        if text[i] != " ":
+            string += text[i]
+
+        elif text[i] == " " and string != "":
+            answer.append(string)
+            string = ""
+
+    return answer
