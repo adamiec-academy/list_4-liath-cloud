31720e6e3b40bb0bf114d5770793b2fee476ccae
liath-cloud <115594751+liath-cloud@users.noreply.github.com>
2022-11-23 18:32:25 +0000

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,15 +1,21 @@
 def my_split(text):
 
     answer=[]
+    flag = 0
     string = ""
 
     for i in range(len(text)):
 
         if text[i] != " ":
             string += text[i]
+            flag = 1
 
         elif text[i] == " " and string != "":
             answer.append(string)
             string = ""
+            flag = 0
+
+    if flag == 1:
+        answer.append(string)
 
     return answer
